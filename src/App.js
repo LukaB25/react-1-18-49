6337ccbc233b6e21179a54f0756b0891bc80a358
Luka Black <luka.black20@gmail.com>
2024-03-27 17:30:21 +0000

    Add LifeCycles from the learning modules.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-import RenderingLists from "./components/RenderingLists";
+// import RenderingLists from "./components/RenderingLists";
+// import Clock from './components/Clock';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return <div className="App">
-    <RenderingLists />
+    {/* <Clock /> */}
+    {/* <RenderingLists /> */}
+    {/* <LifeCyclesCDM /> */}
+    {/* <LifeCyclesCDU /> */}
+      <LifeCyclesCWU />
   </div>;
 }
 
